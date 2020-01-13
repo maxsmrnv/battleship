5a261020c60cfbe75956fd1ae4a5e53b52560224
nevstafev <evstafev.n.e@gmail.com>
2020-01-13 23:03:03 +0300

    Process ships creation.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,37 @@
 import Game from './game.js';
 import Ship from './ship.js';
 import Player from './player.js';
+import WebSocketServer from 'ws';
+import v4 from 'uuid';
 
-let firstPlayer = new Player([new Ship([1])]);
-let secondPlayer = new Player([new Ship([0, 1])]);
+let players = {};
+let game = null;
 
-let game = new Game(firstPlayer, secondPlayer);
-let result = game.shoot({ coordinates: 1, movieOvner: firstPlayer });
-console.log(result);
+let wsServer = new WebSocketServer.Server({
+  port: 8081
+});
+wsServer.on('connection', ws => {
+  ws.id = v4();
+  players[ws.id] = {
+    socket: ws,
+    player: null
+  };
+  ws.onmessage = message => {
+    let data = JSON.parse(message.data);
+    if (data['ships']) {
+      let player = new Player(data.ships.map(ship => new Ship(ship)));
+      players[ws.id].player = player;
+      if (Object.keys(players).length == 2) {
+        game = new Game(
+          players[Object.keys(players)[0]].player,
+          players[Object.keys(players)[1]].player
+        );
+      }
+    } else if (data['shoot']) {
+      game.shoot({
+        movieOvner: players[ws.id].player,
+        coordinates: data.shoot.coordinates
+      });
+    }
+  };
+});
